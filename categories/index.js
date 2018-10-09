2fb8f25805f8c674f80393cb1ba13562f8cc416f
Boimb <atboimb@gmail.com>
2018-10-09 22:04:24 +0200

    added imperihome types

--- categories/index.js
+++ categories/index.js
@@ -1,25 +1,44 @@
 export default {
+  security: {
+    camera: 'security-camera',
+    horn: 'security-horn',
+    bell: 'security-bell',
+    window: 'security-window',
+    doorLock: 'security-door',
+  },
   sensors : {
     temperature: 'temperature-sensor',
-    humidity: 'humidity-sensor'
-    // ...
+    humidity: 'humidity-sensor',
+    hygro: 'hygro-sensor'
+    dioxyde: 'co2-sensor',
+    monoxyde: 'co-sensor',
+    electricConsumption: 'electric-consumption-sensor',
+    flood: 'flood-sensor',
+    luminance: 'luminance-sensor',
+    motion: 'motion-sensor',
+    noise: 'noise-sensor',
+    pressure: 'pressure-sensor',
+    rain: 'rain-sensor',
+    smoke: 'smoke-sensor',
+    ultravioletSensor: 'ultraviolet-sensor',
+    wind: 'wind-sensor',
   },
-  switches: {
-    plug: 'plug',
-    oneWay: 'one-way-switch',
-    twoWay: 'two-way-switch',
-    // ...
+  actuator: {
+    toggle: 'toggle-switch',
+    push: 'push-switch',
+    step: 'step-switch'
   },
   media: {
     tv: 'television',
     amplifier: 'amplifier',
     phone: 'phone',
+    videoRenderer: 'video-renderer',
+    audioRenderer: 'audio-renderer',
+    library: 'library',
   },
   light: {
-    switch: 'light-switch',
-    brightness: 'light-brightness'
+    brightness: 'light-brightness',
+    dimmer: 'light-dimmer',
     color: 'light-color',
-    // ...
-  }
-  // ...
+  },
 }
