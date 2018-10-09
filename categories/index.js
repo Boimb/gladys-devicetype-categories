8684bf7bfe693b8859a5e30261b81ec4bf59aaf3
Boimb <atboimb@gmail.com>
2018-10-09 14:43:12 +0200

    Create index.js

---
+++ categories/index.js
@@ -0,0 +1,25 @@
+export default {
+  sensors : {
+    temperature: 'temperature-sensor',
+    humidity: 'humidity-sensor'
+    // ...
+  },
+  switches: {
+    plug: 'plug',
+    oneWay: 'one-way-switch',
+    twoWay: 'two-way-switch',
+    // ...
+  },
+  media: {
+    tv: 'television',
+    amplifier: 'amplifier',
+    phone: 'phone',
+  },
+  light: {
+    switch: 'light-switch',
+    brightness: 'light-brightness'
+    color: 'light-color',
+    // ...
+  }
+  // ...
+}
